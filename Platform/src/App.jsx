919ba30914e00fe66e55d45b040b42e0ed83bd8e
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-06-07 11:37:18 +0200

    Styling countdouwn

--- Platform/src/App.jsx
+++ Platform/src/App.jsx
@@ -3,12 +3,41 @@ import "./App.css";
 import Countdown from "react-countdown";
 
 function App() {
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return <h1>You are good to gooo</h1>;
+    } else {
+      return (
+        <div style={{ display: "flex", gap: "25px" }} className="timer">
+          <div className="day">
+            <h1>{days}</h1>
+            Days
+          </div>
+          <div className="hour">
+            <h1>{hours}</h1>
+            Hours
+          </div>
+          <div className="min">
+            <h1>{minutes}</h1>
+            Minutes
+          </div>
+          <div className="sec">
+            <h1>{seconds}</h1>
+            Seconds
+          </div>
+        </div>
+      );
+    }
+  };
   return (
     <div className="wrapper">
       <div className="container">
         <h1>COMING SOON..</h1>
         <p>FINAL SHOW 2023</p>
-        <Countdown date={new Date("2023-06-23T19:00:00").getTime()} />
+        <Countdown
+          renderer={renderer}
+          date={new Date("2023-06-23T19:00:00").getTime()}
+        />
       </div>
       <div className="media">
         <h1>Follow our socials for updates !</h1>

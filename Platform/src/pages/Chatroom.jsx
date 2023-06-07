45e0224c6ea1168884a64429f801b56910da4872
Rowanbiets <rowan.biets@student.ehb.be>
2023-06-07 11:54:00 +0200

    structuur

---
+++ Platform/src/pages/Chatroom.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Chatroom = () => {
+  return (
+    <div>Chatroom</div>
+  )
+}
+export default Chatroom()
---
+++ Platform/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styling/home.css'
+export const Home = () => {
+  return (
+    <div>
+        <h1>Final Show</h1>
+
+    </div>
+  )
+}
+export default Home()

148a0f2cd7203ad9139e44d8007fd4cc8eb1b065
biekeb <91184065+biekeb@users.noreply.github.com>
2023-06-07 14:08:37 +0200

    Adding the 3D model to the homepage.

--- Platform/src/pages/Home.jsx
+++ Platform/src/pages/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import '../styling/home.css'
-export const Home = () => {
-  return (
-    <div>
-        <h1>Final Show</h1>
+import React from 'react';
+import '../styling/home.css';
+import { Buildings } from '../components/Buildings';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-    </div>
-  )
-}
-export default Home()
+
+const Home = () => {
+  return (
+    <Canvas>
+            <ambientLight intensity={1} />
+            <pointLight position={[10, 10, 10]} />
+
+            <OrbitControls />
+          
+        
+
+      <Buildings />
+    </Canvas>
+  );
+};
+
+export default Home;

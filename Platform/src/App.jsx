6ed92c58f0b1de4dac0329472bad4d078acab36f
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-06-07 09:49:44 +0200

    Making the construction page

--- Platform/src/App.jsx
+++ Platform/src/App.jsx
@@ -1,9 +1,21 @@
-
 import "./App.css";
 
 function App() {
-
-  return <>IN CONSTRUCTION</>;
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <h1>COMING SOON..</h1>
+        <p>FINAL SHOW 2023</p>
+      </div>
+      <div className="media">
+        <h1>Follow our socials for updates !</h1>
+        <div className="media_buttons">
+          <button>Instagram</button>
+          <button>LinkedIn</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;

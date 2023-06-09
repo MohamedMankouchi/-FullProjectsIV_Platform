7fd04ee605f2be5350077b4ca67f491b20855f7a
Rowanbiets <rowan.biets@student.ehb.be>
2023-06-09 15:21:43 +0200

    routing changes

--- Platform/src/pages/Info.jsx
+++ Platform/src/pages/Info.jsx
@@ -21,8 +21,24 @@ export const Info = () => {
 
 <div className='info3'>
 <h2>INFODAG</h2>
+<p>Zin om langs te komen op onze campus? Het kan! Op zaterdag 25 juni gooien wij onze deuren open tot 15u, en jij bent uitgenodigd! Kom eens proeven van het leven als een student Multimedia & Creatieve Technologie... en wie weet begin jij in september aan onze opleiding!
+</p>
+<h2>Wanneer?</h2>
+<p>Zaterdag 24 juni van 10u tot 15u.</p>
+<h2>Waar?</h2>
+<p>Campus Kaai, Nijverheidskaai 170, 1070 Anderlecht.
+</p>
+</div>
+<div className='info4'>
+<h2>EXPO</h2>
+<p>Welkom bij de Final Show expositie! Hier vind je verschillende projecten van afstuderende studenten in alle vier categorieën: Web & App, Smart Technology, Motion en Extended Reality. Volg de wegwijzers richting de parking van campus Kaai.
 
-
+Geraak je niet op de campus, maar wil je toch de eindwerken bekijken? Dan hebben we goed nieuws, want alle eindwerken zijn ook online te bekijken in onze showroom!</p>
+<h2>WANNEER?</h2>
+<p>Vrijdag 23 juni.
+Zaterdag 25 juni van 10u tot 13u.</p>
+<h2>WAAR?</h2>
+<p>Campus Kaai naast het Medialab, Nijverheidskaai 170, 1070 Anderlecht.</p>
 </div>
     </div>
   )

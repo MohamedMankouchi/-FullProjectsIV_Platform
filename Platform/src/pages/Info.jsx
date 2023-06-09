7f32646fc15c75859def6327d5078d12b4e21f5c
Rowanbiets <rowan.biets@student.ehb.be>
2023-06-09 14:57:36 +0200

    INFO

--- Platform/src/pages/Info.jsx
+++ Platform/src/pages/Info.jsx
@@ -6,9 +6,24 @@ export const Info = () => {
   return (
     <div>
       <HamburgerMenu/>
-
+<div className='info'>
       <h1>MULTIMEDIA & CREATIEVE TECHNOLOGIE</h1>
+      <p>Gepassioneerd door die steeds veranderende geconnecteerde wereld? Ben jij een digitale maker van de toekomst? Of ben je gewoon benieuwd naar wat onze studenten Multimedia & Creatieve Technologie verwezenlijkt hebben dit jaar? Dan ben je hier op het juiste adres!</p>
+      </div>
 
+<div className='info2'>
+  <h2>Livestream</h2>
+<p>Op vrijdag 23 juni worden de beste werken van onze studenten gepresenteerd tijdens onze jaarlijkse Final Show: een show boordevol talent, interviews, en nog veel meer! Tijdens deze livestream worden ook de winnaars aangekondigd voor elke categorie, dit wil je dus zeker niet missen!
+</p>
+  <h2>Wanneer?</h2>
+  <p>23 juni om 19u.</p>
+</div>
+
+<div className='info3'>
+<h2>INFODAG</h2>
+
+
+</div>
     </div>
   )
 }

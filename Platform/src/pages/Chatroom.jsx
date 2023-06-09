d5994af0a769c369ed71c270b7bf1b1ee36ebe65
Rowanbiets <rowan.biets@student.ehb.be>
2023-06-09 14:22:21 +0200

    info page startup

--- Platform/src/pages/Chatroom.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Chatroom = () => {
-  return (
-    <div>Chatroom</div>
-  )
-}
-export default Chatroom()
---
+++ Platform/src/pages/Info.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HamburgerMenu } from "../components/HamburgerMenu";
+import './../styling/info.css'
+
+export const Info = () => {
+  return (
+    <div>
+      <HamburgerMenu/>
+
+      <h1>MULTIMEDIA & CREATIEVE TECHNOLOGIE</h1>
+
+    </div>
+  )
+}
+export default Info()

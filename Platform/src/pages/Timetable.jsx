c1a1dd35f90c1f94d07561caf6269d3ee10c2b79
biekeb <91184065+biekeb@users.noreply.github.com>
2023-06-12 15:03:05 +0200

    adding style to the timetable

--- Platform/src/pages/Timetable.jsx
+++ Platform/src/pages/Timetable.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { HamburgerMenu } from '../components/HamburgerMenu'
+import "../styling/timetable.css";
+import background from '../images/timetableback.png'
 
 
 export const Timetable = () => {
   return (
-    <div>Timetable</div>
+     <div className='timetable'>
+          <HamburgerMenu/>
+
+   <h1>timetable</h1>
+   </div>
   )
 }
 export default Timetable()

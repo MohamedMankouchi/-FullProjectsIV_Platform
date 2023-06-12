017a2516ed6bb803cde7e6e0a07e65c41b01cc84
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-06-12 19:36:11 +0200

    Livestream page rendering stream dynamically

--- Platform/src/pages/Livestream.jsx
+++ Platform/src/pages/Livestream.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import Countdown from "react-countdown";
 import { HamburgerMenu } from "../components/HamburgerMenu";
@@ -7,9 +7,10 @@ import { Link } from "react-router-dom";
 import Instagram from "../images/instagram.svg";
 import { Video } from "../components/Video";
 export const Livestream = () => {
+  const [stream, setStream] = useState(false);
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
-      return <h1>You are good to gooo</h1>;
+      setStream(true);
     } else {
       return (
         <div className="timer">
@@ -40,42 +41,46 @@ export const Livestream = () => {
   return (
     <div className="outerwrapper">
       <HamburgerMenu className={"headerLive"} />
-      {/* <Video /> */}
-      <div className="wrapper">
-        <div className="container">
-          <h1>The show begins in...</h1>
-          <Countdown
-            renderer={renderer}
-            date={new Date("2023-06-23T19:00:00").getTime()}
-          />
-        </div>
-        <div className="media">
-          <p>Be ready on Friday June 23th.</p>
-          <div className="media_buttons">
-            <Link
-              to={
-                "https://www.eventbrite.com/e/tickets-final-show-2023-648852223467"
-              }
-              target="_blank"
-            >
-              {" "}
-              <button className="button-tickets">Get your tickets</button>
-            </Link>
-            <div className="instagram">
-              <p>Follow us on : </p>
-              <div className="instagram_container">
-                <Link
-                  to={"https://www.instagram.com/multimedia.ehb/"}
-                  target="_blank"
-                >
-                  {" "}
-                  <img className="instagram_styling" src={Instagram} />
-                </Link>
+
+      {stream ? (
+        <Video />
+      ) : (
+        <div className="wrapper">
+          <div className="container">
+            <h1>The show begins in...</h1>
+            <Countdown
+              renderer={renderer}
+              date={new Date("2023-06-23T19:00:00").getTime()}
+            />
+          </div>
+          <div className="media">
+            <p>Be ready on Friday June 23th.</p>
+            <div className="media_buttons">
+              <Link
+                to={
+                  "https://www.eventbrite.com/e/tickets-final-show-2023-648852223467"
+                }
+                target="_blank"
+              >
+                {" "}
+                <button className="button-tickets">Get your tickets</button>
+              </Link>
+              <div className="instagram">
+                <p>Follow us on : </p>
+                <div className="instagram_container">
+                  <Link
+                    to={"https://www.instagram.com/multimedia.ehb/"}
+                    target="_blank"
+                  >
+                    {" "}
+                    <img className="instagram_styling" src={Instagram} />
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };

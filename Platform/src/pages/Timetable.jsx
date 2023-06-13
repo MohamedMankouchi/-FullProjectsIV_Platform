e027b4d998466302342d592ac862a8459c3702d7
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-06-13 20:03:36 +0200

    Animated the timetable content

--- Platform/src/pages/Timetable.jsx
+++ Platform/src/pages/Timetable.jsx
@@ -1,37 +1,62 @@
-import React from 'react'
-import { HamburgerMenu } from '../components/HamburgerMenu'
+import React from "react";
+import { HamburgerMenu } from "../components/HamburgerMenu";
 import "../styling/timetable.css";
-import background from '../images/timetableback.png'
-import poster1 from '../images/poster1.png'
-
+import background from "../images/timetableback.png";
+import poster1 from "../images/poster1.png";
+import { motion } from "framer-motion";
 
 export const Timetable = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 1 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.4,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+      },
+    },
+  };
   return (
-    
-     <div className='timetable'>
-          <HamburgerMenu/>
+    <div className="timetable">
+      <HamburgerMenu />
 
-   <div className='timetable-cover'>
-    <div className='tb-poster'>
-      <img src={poster1} alt="" />
+      <div className="timetable-cover">
+        <div className="tb-poster">
+          <img src={poster1} alt="" />
+        </div>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          className="timetable-info"
+        >
+          <motion.h3 variants={item}>Vrijdag 23 juni</motion.h3>
+          <motion.p variants={item}>17:00 Opening deuren </motion.p>
+          <motion.p variants={item}>17:10 Expo</motion.p>
+          <motion.p variants={item}>18:45 Countdown</motion.p>
+          <motion.p variants={item}>19:00 Livestream Final Show</motion.p>
+          <motion.p variants={item}>20:15 Receptie / Expo</motion.p>
+          <motion.p variants={item}>21:00 Afsluit</motion.p>
+          <motion.h3 variants={item}>Zaterdag 24 juni</motion.h3>
+          <motion.p variants={item}>10:00 Start infodag</motion.p>
+          <motion.p variants={item}>10:30 Expo</motion.p>
+          <motion.p variants={item}>15:00 Einde infodag</motion.p>
+        </motion.div>
+      </div>
     </div>
-<div className='timetable-info'>
-
-
-  <h3>Vrijdag 23 juni</h3>
-  <p>17:00 Opening deuren </p>
-  <p>17:10 Expo</p>
-  <p>18:45 Countdown</p>
-  <p>19:00 Livestream Final Show</p>
-  <p>20:15 Receptie / Expo</p>
-  <p>21:00 Afsluit</p>
-  <h3>Zaterdag 24 juni</h3>
-  <p>10:00 Start infodag</p>
-  <p>10:30 Expo</p>
-  <p>15:00 Einde infodag</p>
-</div>
-   </div>
-   </div>
-  )
-}
-export default Timetable()
+  );
+};
+export default Timetable();

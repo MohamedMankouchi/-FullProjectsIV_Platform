5e0d4fdd948ae0434c26eec4f900bfc20c7deb0b
biekeb <91184065+biekeb@users.noreply.github.com>
2023-06-15 10:50:17 +0200

    menu closes after tapted

--- Platform/src/components/MobileBtnComp.jsx
+++ Platform/src/components/MobileBtnComp.jsx
@@ -1,34 +1,79 @@
 import React, { useState } from "react";
 import "../styling/home.css";
 import showAllImage from '../icon/plus-icon.png';
+import livestream from'../icon/livestreamicon.png';
+import info from'../icon/infoicon.png';
+import timetable from'../icon/logoicon.png';
+import showcase from'../icon/Question_mark.svg';
 
-const Mobilenav = ({ setPosition }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Mobilenav = ({ onButtonClick }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleInfoButtonClick = () => {
+    onButtonClick([-30, 12, 0]);
+    setShowMenu(false);
+  };
+
+  const handleTimetableButtonClick = () => {
+    onButtonClick([0, 2, -20]);
+    setShowMenu(false);
+  };
+
+  const handleLiveShowButtonClick = () => {
+    onButtonClick([30, 15, 5]);
+    setShowMenu(false);
+  };
+
+  const handleShowroomButtonClick = () => {
+    onButtonClick([15, 50, 15]);
+    setShowMenu(false);
+  };
 
   const handleToggleMenu = () => {
-    setIsOpen(!isOpen);
+    setShowMenu(!showMenu);
   };
 
   return (
     <div className="mobilebtn">
-      <nav className={`menu ${isOpen ? 'show' : ''}`}>
+      <nav className={`menu ${showMenu ? 'show' : ''}`}>
         <input
-          checked={isOpen}
+          checked={showMenu}
           className="menu-toggler"
           type="checkbox"
           onChange={handleToggleMenu}
         />
-        <label htmlFor="menu-toggler">
+        <label htmlFor="menu-toggler" className="plus-icon">
+          +
         </label>
+
         <ul>
           <li className="menu-item">
-            <a className="fas fa-cat" onClick={() => {setPosition([-30, 12, 0]); setIsOpen(false);}}>info</a>
+            <a className="fas fa-cat" onClick={handleTimetableButtonClick}>
+              <div className="image-blend">
+                <img src={info} alt="" />
+              </div>
+            </a>
           </li>
           <li className="menu-item">
-            <a className="fas fa-cookie-bite" onClick={() => {setPosition([0, 2, -20]); setIsOpen(false);}}>Time</a>
+            <a className="fas fa-cookie-bite" onClick={handleInfoButtonClick}>
+              <div className="image-blend">
+                <img src={livestream} alt="" />
+              </div>
+            </a>
           </li>
           <li className="menu-item">
-            <a className="fab fa-earlybirds" onClick={() => {setPosition([30, 15, 5]); setIsOpen(false);}}>Live</a>
+            <a className="fab fa-earlybirds" onClick={handleLiveShowButtonClick}>
+              <div className="image-blend">
+                <img src={info} alt="" />
+              </div>
+            </a>
+          </li>
+          <li className="menu-item">
+            <a className="fab fa-earlybirds" onClick={handleShowroomButtonClick}>
+              <div className="image-blend">
+                <img src={timetable} alt="" />
+              </div>
+            </a>
           </li>
         </ul>
       </nav>
